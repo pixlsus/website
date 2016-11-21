a3f96d9fab19cc72b275dbe5a5a8afacd3ebdaed
Pat David <patdavid@gmail.com>
2016-11-21 12:48:14 -0600

    Get deploy scripts ready to push from Travis CI

--- deploy.sh
+++ deploy.sh
@@ -15,16 +15,125 @@ then
     chmod 600 /tmp/pixls_rsa
     #eval "$(ssh-agent -s)"
     #ssh-add /tmp/deploy_rsa
-    ls -lha
-    rsync --stats -PSauvhe "ssh -i /tmp/pixls_rsa -o StrictHostKeyChecking=no" build/ pixlsus@pixls.us:/home4/pixlsus/pixls-deploy/incoming/
-    if [ $? -eq 0 ]
-    then
-        echo "rsync successful."
-        echo "todo: mv the dir into place"
-        echo "todo: Creating symlinks. (much later)"
-    else
-        echo "rsync failed! :("
-    fi
+#    ls -lha
+#    rsync --stats -PSauvhe "ssh -i /tmp/pixls_rsa -o StrictHostKeyChecking=no" build/ pixlsus@pixls.us:/home4/pixlsus/pixls-deploy/incoming/
+#    if [ $? -eq 0 ]
+#    then
+#        echo "rsync successful."
+#        echo "todo: mv the dir into place"
+#        echo "todo: Creating symlinks. (much later)"
+#    else
+#        echo "rsync failed! :("
+#    fi
+############################
+
+	# This file is pure insanity.
+	# I apologize in advance if you're trying to do something with it.
+
+	# Get server epoch time into TIMEVAR
+	#TIMEVAR=$(ssh pixlsus@pixls.us 'date +%s')
+	TIMEVAR=$(ssh pixlsus@pixls.us 'date +%Y%m%d%H%M')
+	if [ $? -eq 0 ]
+	then
+		echo "TIMEVAR: $TIMEVAR"
+	else
+		echo "error code: $?"
+		echo "failed getting server time, exiting"
+		exit 1
+	fi
+
+	NEWDIR=pixls-$TIMEVAR
+	echo "NEWDIR: $NEWDIR"
+
+	# Get the current, active directory name
+	CURRDIR=$(ssh pixlsus@pixls.us 'cd ~/pixls-deploy/; find . -mindepth 1 -maxdepth 1 -type d -name pixls* -printf "%P\n"')
+	if [ $? -eq 0 ]
+	then
+		echo "CURRDIR: $CURRDIR"
+	else
+		echo "error code: $?"
+		echo "failed getting current directory, exiting"
+		exit 1
+	fi
+
+	# Hardlink copy current directory to new directory
+	# (this is the dir we will rsync against/into
+	ssh pixlsus@pixls.us "cd ~/pixls-deploy/; cp -la $CURRDIR $NEWDIR"
+	if [ $? -eq 0 ]
+	then
+		echo "cp -al successful"
+	else
+		echo "exit code: $?"
+		echo "cp -al failed!"
+		exit 1
+	fi
+
+	# rsync into new directory, pixls-$TIMEVAR/
+	rsync -PSauve ssh --exclude='.DS_Store' build/ pixlsus@pixls.us:/home4/pixlsus/pixls-deploy/$NEWDIR/
+	if [ $? -eq 0 ]
+	then
+		echo "rsync successful."
+	else
+		echo "exit  code: $?"
+		echo "rsync failed!"
+		# failed, so delete the directory
+		echo "cleaning up $NEWDIR"
+		ssh pixlsus@pixls.us "rm -r /home4/pixlsus/pixls-deploy/$NEWDIR"
+		if [ $? -eq 0 ]
+		then
+			echo "Removed ~/pixls-deploy/$NEWDIR"
+		else
+			echo "Uh oh.  Check the directory, I might not have cleaned up"
+		fi
+		exit 1
+	fi
+
+
+	# create a temporary symlink to the new directory
+	ssh pixlsus@pixls.us "ln -s ~/pixls-deploy/$NEWDIR ~/public_html-tmp"
+	if [ $? -eq 0 ]
+	then
+		echo "ln -s successful"
+		echo "created ~/public_html-tmp -> ~/pixls-deploy/$NEWDIR"
+	else
+		echo "exit  code: $?"
+		echo "ln -s failed!"
+		# failed, so delete the directory
+		echo "cleaning up (rm -r $NEWDIR)"
+		ssh pixlsus@pixls.us 'rm -r /home4/pixlsus/pixls-deploy/$NEWDIR'
+		if [ $? -eq 0 ]
+		then
+			echo "Removed ~/pixls-deploy/$NEWDIR"
+		else
+			echo "Uh oh.  Check the directory, I might not have cleaned up"
+		fi
+		exit 1
+	fi
+
+	# Now move tmp symlink to actual public_html
+	ssh pixlsus@pixls.us "mv -Tf ~/public_html-tmp ~/FAKE_public_html"
+	if [ $? -eq 0 ]
+	then
+		echo "mv -Tf public_html-tmp FAKE_public_html successful"
+	else
+		echo "exit  code: $?"
+		echo "Failed to mv -Tf public_html FAKE_public_HTML!"
+		# failed, so handle it
+		echo "Check the symlinks and manually replace if needed"
+		exit 1
+	fi
+
+
+	# At the end, migrate and move old directories
+	ssh pixlsus@pixls.us rm -r "~/pixls-deploy/previous*"
+	ssh pixlsus@pixls.us mv "~/pixls-deploy/$CURRDIR" "~/pixls-deploy/${CURRDIR//pixls/previous}"
+
+
+
+############################
 else
     echo "Not publishing (not on master, or is a PR)."
 fi
+
+
+ 
