45e762e0d669cb2af8f656977d221ac055cd03b9
Pat David <patdavid@gmail.com>
2018-10-22 10:43:00 -0500

    Update upload.sh to reflect new path on webhost

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -PSauve ssh --exclude='.DS_Store' build/ pixlsus@pixls.us:/home4/pixlsus/public_html/
+rsync -PSauve ssh --exclude='.DS_Store' build/ pixlsus@pixls.us:/home/pixlsus/public_html/
