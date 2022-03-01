ee559d0e6ee2c29ecbbdbfd2785359fd7d8e339b
Pat David <patdavid@gmail.com>
2022-03-01 15:12:21 -0600

    Remove metalsmith* extraneous folders

--- metalsmith-src/articles/index.md
+++
@@ -1,6 +0,0 @@
----
-title: "Articles"
-author: "Pat David"
-date: 2015-01-06T11:11:28
-layout: article-index.hbt
----
--- metalsmith-src/js/img-compare.js
+++
@@ -1,5 +0,0 @@
-var trackLocation = function(e){
-    var position = ((e.pageX - this.getBoundingClientRect().left)/this.offsetWidth)*100;
-    console.log( position );
-    if( position <= 100 ){ this.lastElementChild.style.width = position+"%"; }
-}
