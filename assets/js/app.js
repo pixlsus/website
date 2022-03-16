45ce02272bbc9bf640d125ec98cd22636c6126d3
Pat David <patdavid@gmail.com>
2022-03-15 22:03:07 -0500

    Wire up local latest.json to test discuss list

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,21 +39,31 @@ let report = (entries) => {
 let observer = new IntersectionObserver( report, options );
 observer.observe( document.getElementById('lede-hero') );
 
-function initialize( json ){
+function add_discuss_homepage( json ){
   console.log( json );
-  console.log( json.date );
-  console.log( typeof(json) );
+  var ts = document.getElementById('discuss-col');
+
+  json.topic_list.topics.forEach((topic)=> {
+    //console.log(`${topic.fancy_title}`);
+    const h = document.createElement('h4');
+    const a = document.createElement('article');
+    h.innerText = topic.fancy_title;
+
+    a.appendChild( h );
+    ts.appendChild( a );
+  });
 }
 
 /* Testing fetch API */
 //fetch('http://time.jsontest.com')
-fetch('https://discuss.pixls.us/latest.json')
+//fetch('https://discuss.pixls.us/latest.json', { mode: 'no-cors',})
+fetch('http://localhost:1313/latest.json')
   .then( response => {
         if (!response.ok) {
                 throw new Error(`HTTP error: ${response.status}`);
               }
         return response.json();
       })
-  .then( json => initialize(json) )
+  .then( json => add_discuss_homepage(json) )
   .catch( err => console.error(`Fetch problem: ${err.message}`) );
 
